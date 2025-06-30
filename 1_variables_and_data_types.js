134692064a3b55bee9df668b41da3726571238ac
Yashwanth Dodda <144507981+Yashwanth070@users.noreply.github.com>
2025-06-30 23:03:24 +0530

    Add files via upload

---
+++ 1_variables_and_data_types.js
@@ -0,0 +1,11 @@
+let name = "Yash";        // String
+const age = 25;           // Number
+var isStudent = true;     // Boolean
+let country;              // Undefined
+const middleName = null;  // Null
+
+console.log(typeof name);       // string
+console.log(typeof age);        // number
+console.log(typeof isStudent);  // boolean
+console.log(typeof country);    // undefined
+console.log(typeof middleName); // object (quirky JS behavior!)
