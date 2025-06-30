bf1ac08b258c3dc5e76d4951f15656703db6ec64
Yashwanth Dodda <144507981+Yashwanth070@users.noreply.github.com>
2025-06-30 23:04:13 +0530

    Add files via upload

---
+++ 4_looping_numbers.js
@@ -0,0 +1,18 @@
+// For loop: 1 to 10
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// While loop: even numbers 2 to 20
+let j = 2;
+while (j <= 20) {
+  console.log(j);
+  j += 2;
+}
+
+// Do...while loop: 10 down to 1
+let k = 10;
+do {
+  console.log(k);
+  k--;
+} while (k >= 1);
